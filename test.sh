8b0999b804411fc1908528c44fc6d506af8d93e7
John Jetmore <jj33@pobox.com>
2021-01-23 18:17:31 -0500

    t - playing around with verification more

---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+
+
+hostname
+
+echo "test 00255"
+./swaks --to user@host1.nodns.test.swaks.net --from recip@host1.nodns.test.swaks.net --helo hserver   --tls --quit tls --tls-ca-path testing/certs/ca.pem   \
+    --pipe 'testing/server/smtp-server.pl --silent --domain pipe \
+    --cert testing/certs/expired-signed.example.com.crt --key testing/certs/expired-signed.example.com.key \
+    part-0000-connect-standard.txt \
+    part-0101-ehlo-all.txt \
+    part-0200-starttls-basic.txt \
+    part-3000-shutdown-accept.txt \
+  ' | grep "=== TLS peer cert"
+
+echo "test 00263"
+./swaks --to user@host1.nodns.test.swaks.net --from recip@host1.nodns.test.swaks.net --helo hserver \
+  --tls --quit tls --tls-verify-ca --tls-verify-target node.example.com --tls-ca-path testing/certs/ca.pem \
+  --pipe 'testing/server/smtp-server.pl --silent --domain pipe \
+    --cert testing/certs/node.example.com.crt --key testing/certs/node.example.com.key \
+    part-0000-connect-standard.txt \
+    part-0101-ehlo-all.txt \
+    part-0200-starttls-basic.txt \
+    part-3000-shutdown-accept.txt \
+  ' | grep "=== TLS peer cert"
+
