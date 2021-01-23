194fe34f2e7bfc7ba640c2f70b44d23e62d175cb
John Jetmore <jj33@pobox.com>
2021-01-23 18:58:54 -0500

    t

--- test.sh
+++ test.sh
@@ -5,7 +5,18 @@
 
 hostname
 
-echo "test 00255"
+echo "unsigned, valid host name"
+./swaks --to user@host1.nodns.test.swaks.net --from recip@host1.nodns.test.swaks.net --helo hserver \
+  --tls --quit tls --tls-verify-ca --tls-verify-target unsigned.example.com --tls-ca-path testing/certs/ca.pem \
+  --pipe 'testing/server/smtp-server.pl --silent --domain pipe \
+    --cert testing/certs/unsigned.example.com.crt --key testing/certs/unsigned.example.com.key \
+    part-0000-connect-standard.txt \
+    part-0101-ehlo-all.txt \
+    part-0200-starttls-basic.txt \
+    part-3000-shutdown-accept.txt \
+  ' | grep "=== TLS peer cert"
+
+echo "test 00255 (signed but expired)"
 ./swaks --to user@host1.nodns.test.swaks.net --from recip@host1.nodns.test.swaks.net --helo hserver   --tls --quit tls --tls-ca-path testing/certs/ca.pem   \
     --pipe 'testing/server/smtp-server.pl --silent --domain pipe \
     --cert testing/certs/expired-signed.example.com.crt --key testing/certs/expired-signed.example.com.key \
@@ -15,7 +26,7 @@ echo "test 00255"
     part-3000-shutdown-accept.txt \
   ' | grep "=== TLS peer cert"
 
-echo "test 00263"
+echo "test 00263 (signed, target set to --tls-verify-target node.example.com)"
 ./swaks --to user@host1.nodns.test.swaks.net --from recip@host1.nodns.test.swaks.net --helo hserver \
   --tls --quit tls --tls-verify-ca --tls-verify-target node.example.com --tls-ca-path testing/certs/ca.pem \
   --pipe 'testing/server/smtp-server.pl --silent --domain pipe \
