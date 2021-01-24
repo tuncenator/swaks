9c770bc57399bc18af1a5afdf74d34b8710312a6
John Jetmore <jj33@pobox.com>
2021-01-23 19:26:16 -0500

    t

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@
 
 hostname
 
-echo "unsigned, valid host name"
+echo "unsigned, valid host name (expect: fail, pass)"
 ./swaks --to user@host1.nodns.test.swaks.net --from recip@host1.nodns.test.swaks.net --helo hserver \
   --tls --quit tls --tls-verify-ca --tls-verify-target unsigned.example.com --tls-ca-path testing/certs/ca.pem \
   --pipe 'testing/server/smtp-server.pl --silent --domain pipe \
@@ -16,7 +16,17 @@ echo "unsigned, valid host name"
     part-3000-shutdown-accept.txt \
   ' | grep "=== TLS peer cert"
 
-echo "test 00255 (signed but expired)"
+echo "unsigned and expired (expect: fail, fail)"
+./swaks --to user@host1.nodns.test.swaks.net --from recip@host1.nodns.test.swaks.net --helo hserver   --tls --quit tls --tls-ca-path testing/certs/ca.pem   \
+    --pipe 'testing/server/smtp-server.pl --silent --domain pipe \
+    --cert testing/certs/expired-unsigned.example.com.crt --key testing/certs/expired-unsigned.example.com.key \
+    part-0000-connect-standard.txt \
+    part-0101-ehlo-all.txt \
+    part-0200-starttls-basic.txt \
+    part-3000-shutdown-accept.txt \
+  ' | grep "=== TLS peer cert"
+
+echo "test 00255 (signed but expired) (expect: fail, fail)"
 ./swaks --to user@host1.nodns.test.swaks.net --from recip@host1.nodns.test.swaks.net --helo hserver   --tls --quit tls --tls-ca-path testing/certs/ca.pem   \
     --pipe 'testing/server/smtp-server.pl --silent --domain pipe \
     --cert testing/certs/expired-signed.example.com.crt --key testing/certs/expired-signed.example.com.key \
@@ -26,7 +36,7 @@ echo "test 00255 (signed but expired)"
     part-3000-shutdown-accept.txt \
   ' | grep "=== TLS peer cert"
 
-echo "test 00263 (signed, target set to --tls-verify-target node.example.com)"
+echo "test 00263 (signed, target set to --tls-verify-target node.example.com) (expect pass, pass)"
 ./swaks --to user@host1.nodns.test.swaks.net --from recip@host1.nodns.test.swaks.net --helo hserver \
   --tls --quit tls --tls-verify-ca --tls-verify-target node.example.com --tls-ca-path testing/certs/ca.pem \
   --pipe 'testing/server/smtp-server.pl --silent --domain pipe \
